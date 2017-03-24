3b70b98b07e49f472ce5d95da4fe422b57a55729
User <ibfly@mail.ru>
2017-03-24 16:17:08 +0200

    Проверка вынесена в отдельный метод

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.UnknownHostException;
 
@@ -15,20 +16,35 @@ public class Main {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String urlString = reader.readLine();
 
+        System.out.println(valid(urlString));
+
+    }
+    private static boolean valid(String u) {
+        boolean valid = false;
         try {
-            URL url = new URL(urlString);
+            URL url = new URL(u);
             HttpURLConnection huc = (HttpURLConnection) url.openConnection();
             huc.setInstanceFollowRedirects(false);
             int responseCode = huc.getResponseCode();
-            
-            if (responseCode != 404)
-                System.out.println("Сайт существует");
-            else
-                System.out.println("Такого сайта нет");
+
+            if (responseCode >= 400 && responseCode < 600 ) {
+                System.out.println(" Не доступен");
+            }
+            else {
+                valid = true;
+                System.out.println("Доступен");
+            }
         }
-        catch (UnknownHostException e){
-            System.out.println("Такого сайта нет");
+        catch (MalformedURLException e){
+            System.out.println("Ошибка протокола");
         }
+        catch (UnknownHostException e) {
+            System.out.println("Сервер не доступен");
+        }
+        catch (Exception e) {
+            System.out.println("Ошибка");
+        }
+        return valid;
     }
 }
 
