87b738774302178f03d37cef55212a87bec66b41
User <ibfly@mail.ru>
2017-03-24 14:10:07 +0200

    Добавлена обработка исключения

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.UnknownHostException;
 
 //     Задача от НИКИТИНА
 public class Main {
@@ -14,13 +15,18 @@ public class Main {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String urlString = reader.readLine();
 
-        URL url = new URL(urlString);
-        HttpURLConnection huc = (HttpURLConnection) url.openConnection();
-        int responseCode = huc.getResponseCode();
-
-        if (responseCode != 404) {
-            System.out.println("Сайт существует");
-        } else {
+        try {
+            URL url = new URL(urlString);
+            HttpURLConnection huc = (HttpURLConnection) url.openConnection();
+            huc.setInstanceFollowRedirects(false);
+            int responseCode = huc.getResponseCode();
+            
+            if (responseCode != 404)
+                System.out.println("Сайт существует");
+            else
+                System.out.println("Такого сайта нет");
+        }
+        catch (UnknownHostException e){
             System.out.println("Такого сайта нет");
         }
     }
