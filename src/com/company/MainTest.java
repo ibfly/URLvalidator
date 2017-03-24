093b0b0e0a7e47b9d867d8713d1f04cf282da4a2
User <ibfly@mail.ru>
2017-03-24 18:49:00 +0200

    Merge branch 'master' of https://github.com/ibfly/URLvalidator
    # Conflicts:
    #	src/com/company/MainTest.java

--- src/com/company/MainTest.java
+++ src/com/company/MainTest.java
@@ -8,8 +8,9 @@ import static org.junit.jupiter.api.Assertions.*;
 class MainTest {
     @org.junit.jupiter.api.Test
     void isUrlValid() {
-
+        assertTrue( Main.isUrlValid( "http://google.com" ) );
+        assertFalse( Main.isUrlValid( "http://zzzgoogle.com" ) );
+        assertFalse( Main.isUrlValid( "#$%^" ) );
 
     }
-
 }
