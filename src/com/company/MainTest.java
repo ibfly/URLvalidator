c997e9e2154bfef2865b061513fb14b2c38606c1
Igor Nikitin <igor_nikitin@valentina-db.com>
2017-03-24 17:33:42 +0200

    add trivial tests for method Main.isUrlValid

--- src/com/company/MainTest.java
+++ src/com/company/MainTest.java
@@ -7,9 +7,11 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class MainTest {
     @org.junit.jupiter.api.Test
-    void isUrlValid() {
-        
 
+    void isUrlValid() {
+        assertTrue( Main.isUrlValid( "http://google.com" ) );
+        assertFalse( Main.isUrlValid( "http://zzzgoogle.com" ) );
+        assertFalse( Main.isUrlValid( "#$%^" ) );
     }
 
 }
