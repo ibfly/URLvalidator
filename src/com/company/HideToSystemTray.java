7118aa999bf50adfe8f730e5e8f27503b760c335
User <ibfly@mail.ru>
2017-03-26 17:59:30 +0300

    Добавлен класс HideToSystemTray

---
+++ src/com/company/HideToSystemTray.java
@@ -0,0 +1,75 @@
+package com.company;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.net.URL;
+import javax.swing.*;
+
+public class HideToSystemTray {
+
+    static void createAndShowGUI() {
+        //Check the SystemTray support
+        if (!SystemTray.isSupported()) {
+            System.out.println("SystemTray не поддерживается");
+            return;
+        }
+        final PopupMenu popup = new PopupMenu();
+        final TrayIcon trayIcon =
+                new TrayIcon(createImage("icon32.png", "tray icon"));
+        final SystemTray tray = SystemTray.getSystemTray();
+
+        // Create a popup menu components
+        MenuItem aboutItem = new MenuItem("О программе");
+//
+        MenuItem exitItem = new MenuItem("Выход");
+
+        //Add components to popup menu
+        popup.add(aboutItem);
+        popup.addSeparator();
+        popup.add(exitItem);
+
+        trayIcon.setPopupMenu(popup);
+
+        try {
+            tray.add(trayIcon);
+        } catch (AWTException e) {
+            System.out.println("TrayIcon не может быть добавлен");
+            return;
+        }
+
+        trayIcon.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null,
+                        "Шо ты наделал");
+            }
+        });
+
+        aboutItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null,
+                        "Дуже крута прога");
+            }
+        });
+        // Моя вставка, установка авторазмера иконки
+        trayIcon.setImageAutoSize(true);
+        //
+        exitItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                tray.remove(trayIcon);
+                System.exit(0);
+            }
+        });
+    }
+    //Obtain the image URL
+    protected static Image createImage(String path, String description) {
+        URL imageURL = HideToSystemTray.class.getResource(path);
+
+        if (imageURL == null) {
+            System.err.println("Resource not found: " + path);
+            return null;
+        } else {
+            return (new ImageIcon(imageURL, description)).getImage();
+        }
+    }
+}
+
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,7 @@
 package com.company;
 
 import javax.swing.*;
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -12,6 +9,9 @@ import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
+import static com.company.HideToSystemTray.createAndShowGUI;
+
 //     Задача от НИКИТИНА
 public class Main {
 
@@ -20,7 +20,7 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 //        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-//        String urlString = reader.readLine();
+//        String urlString = reader.readLine();    /JAVA/URLvalidator/icon32.png
         try {
             readConfig();
         }
@@ -34,7 +34,16 @@ public class Main {
                 else
                     System.out.println(adr + " - Не доступен");
             }
+        UIManager.put("swing.boldMetal", Boolean.FALSE);
+        //Schedule a job for the event-dispatching thread:
+        //adding TrayIcon.
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
     }
+
 
     public static boolean isUrlValid(String u) {
         boolean valid = false;
